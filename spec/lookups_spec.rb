e390cd7f83570ace70bc7298eafd4fee9e42657c
pikesley <github@orgraphone.org>
2015-10-27 16:48:53 +0000

    Formatting

--- spec/lookups_spec.rb
+++ spec/lookups_spec.rb
@@ -1,5 +1,6 @@
 module Blocktrain
   describe Lookups do
+
     it 'knows about car codes' do
       expect(described_class.instance.lookups['car_codes']['A']).to eq '2E64930W'
     end
@@ -7,5 +8,6 @@ module Blocktrain
     it 'is an OpenStruct' do
       expect(described_class.instance.lookups.car_codes['B']).to eq '2E64932W'
     end
+
   end
 end
