533468cfb13d72d7f5bacbd0589e77161edf82d0
pikesley <github@orgraphone.org>
2015-10-27 14:30:57 +0000

    Ugh

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,7 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/support/vcr'
   c.hook_into :webmock
-  c.default_cassette_options                = { :record => :once }
+  c.default_cassette_options = { :record => :once }
   c.allow_http_connections_when_no_cassette = true
 
   [
