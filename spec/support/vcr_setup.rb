e5e72a2fcefeabee767d2439b2c72f969a5b281e
pikesley <github@orgraphone.org>
2015-10-27 14:12:46 +0000

    VCR

---
+++ spec/support/vcr_setup.rb
@@ -0,0 +1,16 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/support/vcr'
+  c.hook_into :webmock
+  c.default_cassette_options                = { :record => :once }
+  c.allow_http_connections_when_no_cassette = true
+
+  [
+    'ES_URL'
+  ].each do |env_var|
+    c.filter_sensitive_data("<#{env_var}>") { ENV[env_var] }
+  end
+
+  c.configure_rspec_metadata!
+end
