e413a8d06f822c8a0577b2a2bc04a4f6dd460a1b
pezholio <pezholio@gmail.com>
2015-11-10 10:10:06 +0000

    Test single signal query

--- spec/blocktrain/aggregation_spec.rb
+++ spec/blocktrain/aggregation_spec.rb
@@ -7,6 +7,12 @@ module Blocktrain
       expect(subject.address_query).to eq('memoryAddress:2E64930W OR memoryAddress:2E64932W OR memoryAddress:2E64934W OR memoryAddress:2E64936W')
     end
 
+    it 'queries a single signal' do
+      subject = described_class.new(from: '2015-09-01 10:00:00Z', to: '2015-09-01 11:00:00Z', interval: '10m', signal: 'train_speed')
+
+      expect(subject.address_query).to eq('memoryAddress:2E491EEW')
+    end
+
     context 'with a subsignal specified' do
 
       it 'queries a subsignal' do
