f05df0fe5a0e54d9210fcbf8c88864a0fbcc8209
James Smith <james@floppy.org.uk>
2015-11-10 12:37:53 +0000

    check aggregation doesn't use limit

--- spec/blocktrain/aggregation_spec.rb
+++ spec/blocktrain/aggregation_spec.rb
@@ -14,3 +14,17 @@ module Blocktrain
 
   end
 end
+
+RSpec.shared_examples "aggregations" do |described_class|
+  
+  it "returns 0 results", :vcr do
+    agg = described_class.new(from: '2015-09-01 10:00:00Z', to: '2015-09-01 11:00:00Z', signal: 'passenger_load')
+    expect(agg.send(:result)['hits']['hits'].count).to eq(0)
+  end
+  
+  it "returns 0 results even if a limit is specified", :vcr do
+    agg = described_class.new(from: '2015-09-01 10:00:00Z', to: '2015-09-01 11:00:00Z', signal: 'passenger_load', limit: 1000)
+    expect(agg.send(:result)['hits']['hits'].count).to eq(0)
+  end
+  
+end
