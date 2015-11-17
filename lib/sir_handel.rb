40045e2c8172885e1b0c05d3ab8654d026022617
pikesley <github@orgraphone.org>
2015-11-17 15:50:39 +0000

    Noddy REST-ish URLs

--- lib/sir_handel.rb
+++ lib/sir_handel.rb
@@ -55,6 +55,42 @@ module SirHandel
       end
     end
 
+    get '/signal/:signal' do
+      protected!
+
+      respond_to do |wants|
+        wants.html do
+          @signals = Blocktrain::Lookups.instance.aliases.delete_if {|k,v| v.nil? }
+          @signal = params['signal']
+          erb :weight, layout: :default
+        end
+
+        wants.json do
+          headers 'Access-Control-Allow-Origin' => '*'
+
+          search = {
+            from: params.fetch('from', '2015-09-01 00:00:00Z'),
+            to: params.fetch('to', '2015-09-02 00:00:00Z'),
+            interval: params.fetch('interval', '1h'),
+            signals: params.fetch('signal')
+          }
+
+          r = Blocktrain::Aggregations::AverageAggregation.new(search).results
+
+          results = r['results']['buckets'].map do |r|
+            {
+              'timestamp' => DateTime.strptime(r['key'].to_s, '%Q'),
+              'value' => r['average_value']['value']
+            }
+          end
+
+          {
+            results: results
+          }.to_json
+        end
+      end
+    end
+
     # start the server if ruby file executed directly
     run! if app_file == $0
   end
