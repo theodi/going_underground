793dc3150983fd39a4cda656aa7f1c81fa20b0b0
pezholio <pezholio@gmail.com>
2015-11-20 12:31:37 +0000

    Make index configurable

--- spec/blocktrain/client_spec.rb
+++ spec/blocktrain/client_spec.rb
@@ -1,6 +1,6 @@
 module Blocktrain
   describe Client do
-    
+
     let(:query) {
       JSON.parse File.read 'spec/support/fixtures/query.json'
     }
@@ -9,5 +9,16 @@ module Blocktrain
       expect(described_class.results(query)).to have_key 'aggregations'
     end
 
+    it 'provides a default index' do
+      allow(ENV).to receive(:[]).with('ES_INDEX') { nil }
+
+      expect(described_class.index).to eq('train_data')
+    end
+
+    it 'allows the index to be configured' do
+      allow(ENV).to receive(:[]).with('ES_INDEX') { 'my_cool_index' }
+
+      expect(described_class.index).to eq('my_cool_index')
+    end
   end
 end
