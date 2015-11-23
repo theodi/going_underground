80b81b0702d5ea17c0b474ef743f0ad44e8d1fb2
pikesley <github@orgraphone.org>
2015-11-23 17:51:39 +0000

    Tidy up here

--- features/step_definitions/sir_handel_steps.rb
+++ features/step_definitions/sir_handel_steps.rb
@@ -1,18 +1,18 @@
-And(/^I do not authenticate$/) do
+And /^I do not authenticate$/ do
 
 end
 
-Given(/^I have the following signals$/) do |string|
+Given /^I have the following signals$/ do |string|
   allow(Blocktrain::Lookups.instance).to receive(:aliases) { YAML.load(string) }
 end
 
-Given(/^I have the following Cromulent Dates$/) do |json|
+Given /^I have the following Cromulent Dates$/ do |json|
   allow_any_instance_of(Redis).to receive(:get) {
     json
   }
 end
 
-Given(/^I have no Cromulent Dates$/) do
+Given /^I have no Cromulent Dates$/ do
   allow_any_instance_of(Redis).to receive(:get) {
     nil
   }
