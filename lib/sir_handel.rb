8158a92b2ed3e9e13e228b69dd6cffa4e8e5b788
pezholio <pezholio@gmail.com>
2015-12-14 10:40:28 +0000

    Formatting

--- lib/sir_handel.rb
+++ lib/sir_handel.rb
@@ -25,13 +25,13 @@ module SirHandel
     end
 
     cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                    {:username => ENV["MEMCACHIER_USERNAME"],
-                     :password => ENV["MEMCACHIER_PASSWORD"],
-                     :failover => true,
-                     :socket_timeout => 1.5,
-                     :socket_failure_delay => 0.2,
-                     :pool_size => 5
-                    })
+                              {:username => ENV["MEMCACHIER_USERNAME"],
+                               :password => ENV["MEMCACHIER_PASSWORD"],
+                               :failover => true,
+                               :socket_timeout => 1.5,
+                               :socket_failure_delay => 0.2,
+                               :pool_size => 5
+                              })
 
     set :cache, cache
     set :public_folder, 'public'
