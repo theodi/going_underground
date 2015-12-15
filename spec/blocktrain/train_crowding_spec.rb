6bd32096dc42a93526ce9c0aff63cbe3b023dc92
pikesley <github@orgraphone.org>
2015-12-15 15:31:44 +0000

    We now take a whole day of data

--- spec/blocktrain/train_crowding_spec.rb
+++ spec/blocktrain/train_crowding_spec.rb
@@ -1,8 +1,8 @@
 module Blocktrain
   describe TrainCrowding do
     it 'sends the right paramesters to ATPQuery' do
-      from = '1974-06-15T00:00:00+00:00'
-      to = '1974-06-15T02:00:00+00:00'
+      from = '1974-06-14T00:00:00+00:00'
+      to = '1974-06-15T00:00:00+00:00'
 
       expect(ATPQuery).to receive(:new).with(from: from, to: to, station: :euston, direction: :southbound)
 
