fff115154e38d9e0d7fdb9392beed4f4c5f1600e
pezholio <pezholio@gmail.com>
2015-12-15 16:46:54 +0000

    Check correct url

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -179,7 +179,7 @@ module SirHandel
     it 'allows the datetime to be set', :vcr do
       to = "2015-09-23T17:10:00Z"
       expect(Blocktrain::TrainCrowding).to receive(:new).with(Time.parse(to), "seven_sisters", :southbound).and_call_original
-      get "stations/arriving/southbound/seven-sisters.json?to=#{to}"
+      get "stations/arriving/southbound/seven-sisters/#{to}.json"
     end
 
     it 'shows all stations' do
