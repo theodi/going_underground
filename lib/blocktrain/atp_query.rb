75227c4a03a508d0c0100aefc7acfa6cd50830c9
pezholio <pezholio@gmail.com>
2015-12-15 10:41:46 +0000

    Excise cruft

--- lib/blocktrain/atp_query.rb
+++ lib/blocktrain/atp_query.rb
@@ -13,22 +13,6 @@ module Blocktrain
       super(options)
     end
 
-    def filtered_filter
-      super.tap do |f|
-        f[:bool][:must] << [
-          station_filter,
-          segments_filter
-        ]
-      end
-    end
-
-    def body
-      super.tap do |q|
-        q[:sort] = sort
-      end
-    end
-
-
     def build_query(addresses)
       "memoryAddress:#{addresses.first} AND value:#{station_filter}"
     end
@@ -44,28 +28,6 @@ module Blocktrain
 
     def stations
       YAML.load_file File.join('config', 'stations.yml')
-    end
-
-    def segments_filter
-      script = case direction
-      when :northbound
-        "doc['value'].value % 2 == 0"
-      when :southbound
-        "doc['value'].value % 2 == 1"
-      end
-      {
-        script: {
-          script: script,
-          lang: 'expression'
-        }
-      }
-    end
-
-    def output
-      results.map {|i| i['_source']}.each do |i|
-        puts [i['timeStamp'].ljust(25), i['value'], i['runLengthMs']].join("\t")
-      end
-      nil
     end
 
   end
