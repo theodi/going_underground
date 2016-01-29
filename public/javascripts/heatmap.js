6d2c0adad4fb3e972132f56c0f9ba96e0a44cee8
pezholio <pezholio@gmail.com>
2016-01-29 10:47:08 +0000

    Use a scale of colours from green to red

--- public/javascripts/heatmap.js
+++ public/javascripts/heatmap.js
@@ -8,10 +8,16 @@ function loadHeatmap(url) {
     json.forEach(function(data) {
       count = Math.ceil(data.load / 10)
       $('#'+ data.direction +' .'+ data.station + ' a').append('<div id="indicator" class="weight-'+ count +'"></div>')
-      $('#'+ data.direction +' .'+ data.station +' #indicator').append('<span class="block" style="background-color:'+ colour(data.load) +'; width: '+ data.load +'%"></span>');
+      $('#'+ data.direction +' .'+ data.station +' #indicator').append('<span class="block" style="background-color:'+ getColor(data.load) +'; width: '+ data.load +'%"></span>');
     })
     deferred.resolve();
   });
 
   return deferred.promise();
+}
+
+function getColor(value){
+    value = value / 100
+    var hue=((1-value)*120).toString(10);
+    return ["hsla(",hue,",100%,50%,0.8)"].join("");
 }
