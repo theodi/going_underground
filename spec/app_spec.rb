cef74f6222286c48915d8057043b07063158ce6a
pezholio <pezholio@gmail.com>
2016-02-18 15:45:33 +0000

    Remove this

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -240,51 +240,5 @@ module SirHandel
       expect(last_request.url).to eq 'http://example.org/heatmap/2015-09-03T10:00:00+00:00'
     end
 
-    it 'gets a result for a heatmap for a time period', :vcr do
-      get '/heatmap/2015-09-23T17:00:00Z/2015-09-23T17:30:00Z.json'
-
-      json = JSON.parse(last_response.body)
-
-      expect(json.count).to eq(7)
-      expect(json.first).to eq([
-        {"segment"=>1894, "station"=>"brixton", "direction"=>"northbound", "load"=>11.409491038022043},
-        {"segment"=>1999, "station"=>"stockwell", "direction"=>"southbound", "load"=>27.70859151599568},
-        {"segment"=>1881, "station"=>"vauxhall", "direction"=>"southbound", "load"=>34.87608467004879},
-        {"segment"=>1793, "station"=>"pimlico", "direction"=>"southbound", "load"=>53.23052300174797},
-        {"segment"=>1681, "station"=>"victoria", "direction"=>"southbound", "load"=>57.001145337653135},
-        {"segment"=>1565, "station"=>"green_park", "direction"=>"southbound", "load"=>97.07543448715087},
-        {"segment"=>1487, "station"=>"oxford_circus", "direction"=>"southbound", "load"=>94.94368275515336},
-        {"segment"=>893, "station"=>"seven_sisters", "direction"=>"southbound", "load"=>24.849155220589473},
-        {"segment"=>297, "station"=>"tottenham_hale", "direction"=>"southbound", "load"=>10.826860508464282},
-        {"segment"=>195, "station"=>"blackhorse_road", "direction"=>"southbound", "load"=>5.4407673395445135},
-        {"segment"=>119, "station"=>"walthamstow_central", "direction"=>"southbound", "load"=>4.4395012689378595},
-        {"segment"=>600, "station"=>"finsbury_park", "direction"=>"northbound", "load"=>70.67649122807018},
-        {"segment"=>870, "station"=>"highbury_and_islington", "direction"=>"northbound", "load"=>86.94923155737705},
-        {"segment"=>1026, "station"=>"kings_cross_st_pancras", "direction"=>"northbound", "load"=>89.4342820479},
-        {"segment"=>1282, "station"=>"euston", "direction"=>"northbound", "load"=>82.932464411741},
-        {"segment"=>1358, "station"=>"warren_street", "direction"=>"northbound", "load"=>86.75000130493461},
-        {"segment"=>1444, "station"=>"green_park", "direction"=>"northbound", "load"=>71.93170006408403},
-        {"segment"=>1522, "station"=>"victoria", "direction"=>"northbound", "load"=>62.61486433754038},
-        {"segment"=>1642, "station"=>"pimlico", "direction"=>"northbound", "load"=>29.57324973182908},
-        {"segment"=>1766, "station"=>"vauxhall", "direction"=>"northbound", "load"=>17.181602698739795},
-        {"segment"=>1878, "station"=>"stockwell", "direction"=>"northbound", "load"=>13.124603174603173},
-        {"segment"=>2002, "station"=>nil, "direction"=>"northbound", "load"=>18.22630493576741}
-      ])
-      expect(json.last).to eq([
-        {"segment"=>1794, "station"=>"vauxhall", "direction"=>"northbound", "load"=>12.151785714285714},
-        {"segment"=>1892, "station"=>"stockwell", "direction"=>"northbound", "load"=>10.239285714285714},
-        {"segment"=>1999, "station"=>"stockwell", "direction"=>"southbound", "load"=>30.084578161003556},
-        {"segment"=>1881, "station"=>"vauxhall", "direction"=>"southbound", "load"=>43.350831430318934},
-        {"segment"=>1793, "station"=>"pimlico", "direction"=>"southbound", "load"=>53.23052300174797},
-        {"segment"=>1681, "station"=>"victoria", "direction"=>"southbound", "load"=>57.001145337653135},
-        {"segment"=>1565, "station"=>"green_park", "direction"=>"southbound", "load"=>97.07543448715087},
-        {"segment"=>1487, "station"=>"oxford_circus", "direction"=>"southbound", "load"=>94.94368275515336},
-        {"segment"=>893, "station"=>"seven_sisters", "direction"=>"southbound", "load"=>24.849155220589473},
-        {"segment"=>297, "station"=>"tottenham_hale", "direction"=>"southbound", "load"=>10.826860508464282},
-        {"segment"=>195, "station"=>"blackhorse_road", "direction"=>"southbound", "load"=>5.4407673395445135},
-        {"segment"=>119, "station"=>"walthamstow_central", "direction"=>"southbound", "load"=>4.4395012689378595},
-        {"segment"=>600, "station"=>"finsbury_park", "direction"=>"northbound", "load"=>70.67649122807018}
-      ])
-    end
   end
 end
