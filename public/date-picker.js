419f94f8777bdbe6d34d3dc8550d6c7e5e67d48f
pezholio <pezholio@gmail.com>
2016-03-16 10:32:01 +0000

    Refactor datepicker logic and set value before min / max

--- public/date-picker.js
+++ public/date-picker.js
@@ -1,28 +1,38 @@
 function callDatePicker() {
   $.getJSON('/cromulent-dates', function(json) {
-    var format = 'YYYY-MM-DD HH:mm:ss'
-
-    $('#from-date').datetimepicker({
-      format: format,
-      useCurrent: false,
-      minDate: json.start,
-      maxDate: json.end
-    });
-
-    $('#to-date').datetimepicker({
-      format: format,
-      useCurrent: false,
-      minDate: json.start,
-      maxDate: json.end
-    });
-
-    $('#from-date input').blur(function() {
-      dateFrom = moment(this.value)
-      dateTo = $('#to-date').data("DateTimePicker")
-      dateTo.maxDate(dateFrom.endOf('day').format())
-      dateTo.minDate(dateFrom.startOf('day').format())
-      dateTo.date(dateFrom.add(1, 'hours').format())
-    })
-
+    setDatePickers(json.start, json.end)
   });
+
+  $('#from-date input').blur(function() {
+    updateToDate(moment(this.value))
+  })
+}
+
+function setDatePickers(start, end) {
+  var format = 'YYYY-MM-DD HH:mm:ss'
+
+  $('#from-date').datetimepicker({
+    format: format,
+    useCurrent: false,
+    minDate: start,
+    maxDate: end
+  });
+
+  $('#to-date').datetimepicker({
+    format: format,
+    useCurrent: false,
+    minDate: start,
+    maxDate: end
+  });
+}
+
+function updateToDate(date) {
+  datePicker = $('#to-date').data("DateTimePicker")
+  setDates(datePicker, date)
+}
+
+function setDates(datePicker, date) {
+  datePicker.date(date.add(1, 'hours').format())
+  datePicker.maxDate(date.endOf('day').format())
+  datePicker.minDate(date.startOf('day').format())
 }
